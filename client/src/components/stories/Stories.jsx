4e08eb7bb4e8fc98859321afdd7d08faf8ea72eb
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-05 22:01:27 +0200

    Composant stories fini

--- client/src/components/stories/Stories.jsx
+++ client/src/components/stories/Stories.jsx
@@ -1,7 +1,48 @@
+import { useContext } from "react";
 import "./stories.scss";
+import { AuthContext } from "../../context/authContext";
 
 const Stories = () => {
-  return <div className="stories">Stories</div>;
+  const { currentUser } = useContext(AuthContext);
+  //TEMPORAIRE
+  const stories = [
+    {
+      id: 1,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+
+  return (
+    <div className="stories">
+      <div className="story">
+        <img src={currentUser.profilePic} alt="" />
+        <span>{currentUser.name}</span>
+        <button>🔨</button>
+      </div>
+      {stories.map((story) => (
+        <div className="story">
+          <img src={story.img} alt="" />
+          <span>{story.name}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Stories;
