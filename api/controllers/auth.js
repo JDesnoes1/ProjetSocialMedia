703ecb923a2733bc5fa6cafcd84af635b4d41c2c
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-07 09:10:15 +0200

    Connexion bd, début auth

---
+++ api/controllers/auth.js
@@ -0,0 +1,9 @@
+export const register = (req, res) => {
+  //Vérifier si l'utilisateur existe
+  //Si il existe pas, on créer un nouvel utilisateur
+  //Hash le password
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
