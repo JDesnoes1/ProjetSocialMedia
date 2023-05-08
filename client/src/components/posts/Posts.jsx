d319fd8b5ff2e08cc024d239bb7e22bf6607f001
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-08 09:54:32 +0200

    Fetch insérer un post dans le composant Share

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,13 +4,11 @@ import { useQuery } from "react-query";
 import { makeRequest } from "../../axios";
 
 const Posts = () => {
-  const { isLoading, error, data } = useQuery("posts", () =>
+  const { isLoading, error, data } = useQuery(["posts"], () =>
     makeRequest.get("/posts").then((res) => {
       return res.data;
     })
   );
-
-  console.log(data);
 
   return (
     <div className="posts">
