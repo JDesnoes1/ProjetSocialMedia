ef59c0135b67e709734ad8bbd864bd23d7cadc80
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-08 12:19:23 +0200

    Commentaires finito

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,11 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./comments.scss";
 import { AuthContext } from "../../context/authContext";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { makeRequest } from "../../axios";
 import moment from "moment";
 
 const Comments = ({ postId }) => {
+  const [desc, setDesc] = useState("");
+
   const { currentUser } = useContext(AuthContext);
 
   const { isLoading, error, data } = useQuery(["comments"], () =>
@@ -14,18 +16,43 @@ const Comments = ({ postId }) => {
     })
   );
 
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (newComment) => {
+      return makeRequest.post("/comments", newComment);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries("comments");
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc, postId });
+    setDesc("");
+  };
+
   return (
     <div className="comments">
       <div className="write">
         <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="Ecrire un commentaire" />
-        <button>Envoyer</button>
+        <input
+          type="text"
+          placeholder="Ecrire un commentaire"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <button onClick={handleClick}>Envoyer</button>
       </div>
       {isLoading
         ? "Loading"
         : data.map((comment) => (
-            <div className="comment">
-              <img src={comment.profilePicture} alt="" />
+            <div className="comment" key={comment.id}>
+              <img src={comment.profilePic} alt="" />
               <div className="info">
                 <span>{comment.name}</span>
                 <p>{comment.desc}</p>
